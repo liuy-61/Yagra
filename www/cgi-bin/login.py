2d1b1180fac82a5b89bc516cc57891500bbfb7b6
liuy-61 <1715052124@qq.com>
2022-03-24 15:35:55 +0800

    解决了登录用户名不存在或密码错误时，返回响应出错的bug.

--- www/cgi-bin/login.py
+++ www/cgi-bin/login.py
@@ -9,41 +9,47 @@ from http import cookies
 from mysql import query_data, insert_or_update_data, exist_db, exist_tb_in_db, DBNAME
 cgitb.enable()
 
-print("Content-type:application/json")
-print()
-
 # 获取用户名和密码
 form = cgi.FieldStorage()
 name_front = form.getvalue("name")
 pwd_front = form.getvalue("pwd")
 
 
-sql = 'select user_name,pwd from User where user_name = \"{}\"'.format(name_front)
+sql = 'select user_name,pwd from User where user_name = \"{}\"'.format(
+    name_front)
 res = query_data(sql)
 if len(res) == 0:
-    result = {"status_code": 0,
-              "status_str": "login failed, the user is not registered, please register firstly."
-              }
+    result = {
+        "status_code": 0,
+        "status_str": "login failed, the user is not registered, please register firstly."}
 elif res[0]['pwd'] != pwd_front:
-    result = {"status_code": 1,
-              "status_str": "login failed, wrong password, please login again. "
-              }
+    result = {
+        "status_code": 1,
+        "status_str": "login failed, wrong password, please login again. "}
 else:
     result = {"status_code": 2,
               "status_str": "login successfully "
               }
+print("Content-type:text/html")
+
+# TODO 如果登录成功，服务器生成session_id 以及expire_time
+if result['status_code'] == 2:
+    session_id = "555444666111"
+    GMT_FORMAT = '%a, %d %b %Y %H:%M:%S GMT'
+    expire_time = (datetime.datetime.now() + datetime.timedelta(hours=+1)).strftime(GMT_FORMAT)
+    print('Set-Cookie: session_id={}; expires={}'.format(session_id, expire_time))
+
+print()
 print(json.dumps(result))
 
-    # TODO 服务器生成session_id 以及expire_time
-    # session_id = secrets.token_urlsafe(16)
-    # session_id = "555444666111"
-    # GMT_FORMAT = '%a, %d %b %Y %H:%M:%S GMT'
-    # expire_time = (datetime.datetime.now() + datetime.timedelta(hours=+1)).strftime(GMT_FORMAT)
-
-    # print('Set-Cookie: session_id={}; expires={}'.format(session_id, expire_time))
-    # TODO 将session_id写入cookie，将expire_time作为cookie的有效时间
-    # TODO 浏览器的每次访问都会自动加上cookie用以验证，直到cookie失效
-    # print("login successfully")
+# session_id = secrets.token_urlsafe(16)
+# session_id = "555444666111"
+# GMT_FORMAT = '%a, %d %b %Y %H:%M:%S GMT'
+# expire_time = (datetime.datetime.now() + datetime.timedelta(hours=+1)).strftime(GMT_FORMAT)
+# print('Set-Cookie: session_id={}; expires={}'.format(session_id, expire_time))
+# TODO 将session_id写入cookie，将expire_time作为cookie的有效时间
+# TODO 浏览器的每次访问都会自动加上cookie用以验证，直到cookie失效
+# print("login successfully")
 
 
 # TODO 获取cookie的方式
